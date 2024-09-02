658552e1cb237e8faeae359c1c939feb30145614
AlishaHong <162010393+AlishaHong@users.noreply.github.com>
2024-09-02 10:27:19 +0900

    data추가

--- hello_opencv.py
+++ hello_opencv.py
@@ -9,7 +9,7 @@ print('Hello OpenCV', cv2.__version__)
 # img = cv2.imread('data/lenna.bmp')
 img_gray = cv2.imread('data/puppy.jpg', cv2.IMREAD_GRAYSCALE)
 img_bgr = cv2.imread('data/puppy.jpg')
-print(type(img_bgr))    #넘파이(numpy.ndarray)
+print(type(img_bgr))    #데이터 타입 : 넘파이(numpy.ndarray)
 
 # 파일을 못찾아서 이미지를 못 읽어온 경우 
 # 프로그램 종료 
@@ -30,6 +30,9 @@ cv2.imshow('image_gray', img_gray)
 cv2.waitKey(2000) # 1초만 기다리고 모든창을 닫음
                   # 지연값 없으면 무한 대기 
 
+
+# wait 키를 변수에 담아 다양하게 활용 
+# 's'를 누르면 저장가능
 
 # k = cv2.waitKey()
 
