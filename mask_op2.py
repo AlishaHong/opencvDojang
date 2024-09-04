e1e3c5d58c58c7e33dfbb8bd4b949061d0b5a2b9
AlishaHong <162010393+AlishaHong@users.noreply.github.com>
2024-09-04 17:28:41 +0900

    마스크연산공부

--- mask_op2.py
+++ mask_op2.py
@@ -2,28 +2,28 @@ import numpy as np
 import cv2, sys
 
 # 이미지 불러오기
-logo = cv2.imread('data2/opencv-logo-white.png',cv2.IMREAD_UNCHANGED)
+logo = cv2.imread('data2/opencv-logo-white.png', cv2.IMREAD_UNCHANGED)
 src = cv2.imread('data2/cat.bmp')
 
-# 알파채널 빼고 슬라이싱
-logo = logo[:,:,3]  #알파 뺴기 
-print(logo.shape)
-
-# 알파채널만 슬라이싱
-mask = logo[:,:,:-1]
+# 알파채널 슬라이싱 (알파 채널만 추출)
+mask = logo[:,:,3]
 print(mask.shape)
 
-h,w = mask.shape[:2]
+# 로고의 크기를 얻기
+h, w = mask.shape[:2]
 crop = src[10:10+h, 10:10+w]
 
 # 마스크 연산
-cv2.copyTo(logo,mask,crop)
+cv2.copyTo(logo[:,:,:3], mask, crop)  # 알파 채널이 아닌 RGB 채널을 대상으로 연산
+#cv2.copyTo(src, mask, dst)
 
-
-cv2.imshow('logo',logo)
-cv2.imshow('mask',mask)
-cv2.imshow('src',src)
+# 1. src - 스티커 같은거 
+# 2. 원본이미지에서 스티커만 떼어내야 하므로 스티커를 제외한 부분만 계산
+# 3. 스티커를 붙일 이미지 - 이미지의 영역중 스티커를 붙일 영역을 계산해둠
+# 이미지 출력
+cv2.imshow('logo', logo)
+cv2.imshow('mask', mask)
+cv2.imshow('src', src)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 
